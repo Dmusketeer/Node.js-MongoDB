776dd2dc4051c029289fb50d751e17f449e68a33
Dmusketeer <ardmountronics@gmail.com>
2021-08-05 16:20:29 +0530

    retrieve operations

--- mongoose/Day1/index.js
+++ mongoose/Day1/index.js
@@ -10,31 +10,42 @@ const Learners = new mongoose.Schema({
 });
 
 const learnersDetails = new mongoose.model('Details',Learners);
-const learner_1=new learnersDetails({
+// const learner_1=new learnersDetails({
 
-    Name: "Dheeraj Tiwari",
-    Age: 26,
-    Batch: 'B2',
-    College: "MMMUT",
-    Graduated: true
-});
+//     Name: "Dheeraj Tiwari",
+//     Age: 26,
+//     Batch: 'B2',
+//     College: "MMMUT",
+//     Graduated: true
+// });
 
-const learner_2 = new learnersDetails({
+// const learner_2 = new learnersDetails({
 
-    Name: "Prakhar",
-    Age: 28,
-    Batch: 'B2',
-    College: "KEC",
-    Graduated: true
-});
-const learner_3 = new learnersDetails({
+//     Name: "Prakhar",
+//     Age: 28,
+//     Batch: 'B2',
+//     College: "KEC",
+//     Graduated: true
+// });
+// const learner_3 = new learnersDetails({
 
-    Name: "Rahul",
-    Age: 27,
-    Batch: 'B3',
-    College: "Buddha",
-    Graduated: false
-});
+//     Name: "Rahul",
+//     Age: 27,
+//     Batch: 'B3',
+//     College: "Buddha",
+//     Graduated: false
+// });
 
 // learner_data.save();
-learnersDetails.insertMany([learner_1, learner_2, learner_3]);
+// learnersDetails.insertMany([learner_1, learner_2, learner_3]);
+
+
+// retrieve operations:
+learnersDetails.find((err,data)=>{
+    // err?console.log(err):console.log(data) ;
+    // err ? console.log(err) : console.log(data[1].Name) ;
+    console.log("=======================================")
+    err ? console.log(err) : console.log(data.forEach((da)=>{
+        console.log(da.Name)
+    })) ;
+})
